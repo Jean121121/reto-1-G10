489f887931ee9a3b5eebfdb7725515964400a0b9
JanireG <jgarciadi23dw@ikzubirimanteo.com>
2023-11-30 13:20:21 +0100

    Añadir clase SistemaPuntos

---
+++ programacion/src/SistemaPuntos.java
@@ -0,0 +1,11 @@
+package programacion.src;
+
+public class SistemaPuntos {
+    // Atributos
+    private int puntos;
+
+    // Constructor
+    public SistemaPuntos(int puntos) {
+        this.puntos=puntos;
+    }
+}
